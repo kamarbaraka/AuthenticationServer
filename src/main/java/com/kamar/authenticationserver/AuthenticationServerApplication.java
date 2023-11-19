bd5155fd66ba248595300390e5322fdc113314f3
kamarbaraka <kamar254baraka@gmail.com>
2023-11-19 22:26:07 +0300

    initial configurations.
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

--- src/main/java/com/kamar/authenticationserver/AuthenticationServerApplication.java
+++ src/main/java/com/kamar/authenticationserver/AuthenticationServerApplication.java
@@ -2,12 +2,22 @@ package com.kamar.authenticationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class AuthenticationServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationServerApplication.class, args);
+    }
+
+    TomcatProtocolHandlerCustomizer<?> tomcatProtocolHandlerVirtualThreadCustomizer(){
+
+        return protocolHandler -> protocolHandler.setExecutor(
+                Executors.newVirtualThreadPerTaskExecutor()
+        );
     }
 
 }
